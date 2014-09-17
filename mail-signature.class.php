9bd97bf4527f9b0d64c760e376a96970a79dd0fe
bryan-groupanizer <bryan@groupanizer.com>
2014-09-17 15:15:57 -0700

    logging

--- mail-signature.class.php
+++ mail-signature.class.php
@@ -163,6 +163,8 @@ class mail_signature {
 		$sHeaders = preg_replace("/\n\s+/", " ", $sHeaders);
 		
 		$lines = explode("\r\n", $sHeaders);
+    dpm("lines:");
+    dpm($lines);
 		
 		foreach($lines as $key => $line){
 			
@@ -394,6 +396,7 @@ class mail_signature {
 	
 	// you may leave $to and $subject empty if the corresponding headers are already in $headers
 	public function get_signed_headers($to, $subject, $body, $headers){
+	  dpm("string headers:");
 		dpm($headers);
 		$signed_headers = '';
 		
@@ -412,10 +415,12 @@ class mail_signature {
 				if(!empty($subject)) $headers .= 'Subject: '.$subject."\r\n";
 			}
       
+      dpm("string headers with subject and to concatenated:")
       dpm($headers);
 			
 			// get the clean version of headers used for signature
 			$this -> canonicalized_headers_relaxed = $this -> _dkim_canonicalize_headers_relaxed($headers);
+      dpm("canoicalized headers:");
       dpm($this -> canonicalized_headers_relaxed);
 			
 			if(!empty($this -> canonicalized_headers_relaxed)){
@@ -427,6 +432,7 @@ class mail_signature {
 				
 				if($this -> options['use_dkim'] == true){
 					$signed_headers .= $this -> _get_dkim_header($body);
+          dpm("signed headers:");
           dpm($signed_headers);
 				}
 			}
